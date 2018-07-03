b5c3778d6fb332bb867a73556db846ede79342ff
Ning Wang <ningwang@mlyons-1.u16.stevens-tech.edu>
2018-07-03 15:21:22 -0400

    Initial Commit

---
+++ ios_test_proj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_test_proj
+//
+//  Created by Ning Wang on 7/3/18.
+//  Copyright © 2018 Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
